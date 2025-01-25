4979251570c9536e7c1b059a834648cbbb181bbb
Daniel <danielrotheia@gmail.com>
2025-01-25 11:18:44 -0300

    .

--- apps/audit/admin.py
+++ apps/audit/admin.py
@@ -2,8 +2,11 @@ from django.contrib import admin
 
 from .models import Audit
 
+
 class AdminAudit(admin.ModelAdmin):
     list_display = ('id', 'table', 'object_id', 'updated_at', 'old_values', 'user')
     list_filter = ('table', 'updated_at', 'user')
+    list_per_page = 20
+
 
 admin.site.register(Audit, AdminAudit)
